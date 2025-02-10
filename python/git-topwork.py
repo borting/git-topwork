b2772ef6e4089a1063bdf1ce9bf954147dd2499d
borting <bortingchen@gmail.com>
2025-02-10 17:21:43 +0800

    Add git-topwork.py

---
+++ python/git-topwork.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#
+# Copyright 2025, Borting Chen <bortingchen@gmail.com>
+#
+# This file is licensed under the GPL v3.
+#
+
+
+import git
+import gitdb
+from pathlib import Path
+
